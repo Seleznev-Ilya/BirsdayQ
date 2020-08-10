83f215cd31b294d305fa7d5c7aea141fc11fc513
Ilia Selieznov <ziziron21@gmail.com>
2020-08-11 01:27:39 +0300

    logic finish

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 function Counter() {
     this.count = 0;
-
     this.up = function () {
         return ++this.count;
     };
@@ -8,36 +7,41 @@ function Counter() {
         return --this.count;
     };
 }
-
 let counter = new Counter();
 let counter1 = new Counter();
-let a;
-
-counter1.count = localStorage.getItem('counter1');
-mainButton1.addEventListener('click', switchGame);
-
-function switchGame() {
-    a = +prompt('cod',);
-    if (cod[counter1.count - 1] === a) {
-        f();
-    } else {
+function switchGame1() {
+    if (localStorage.getItem('start') < 2) {
+        localStorage.setItem('start', counter.up());
+        mainContainer.innerText = `${arrStart[localStorage.getItem('start') - 1]}`;
+    } else if (+localStorage.getItem('start') === 2) {
+        let a;
         counter1.count = localStorage.getItem('counter1');
-        mainContainer.innerText = `Задание (${arr[counter1.count - 1]})`;
+        if (!counter1.count) {
+            fCount();
+        } else {
+            a = +prompt('cod',);
+            if (cod[+counter1.count - 1] === a) {
+                fCount();
+            } else {
+                counter1.count = localStorage.getItem('counter1');
+                mainContainer.innerText = `Задание (${arr[counter1.count - 1]})`;
+            }
+        }
+        function fCount() {
+            localStorage.setItem('counter1', counter1.up());
+            mainContainer.innerText = `Задание (${arr[counter1.count - 1]})`;
+        }
     }
 }
-
-function f() {
-    localStorage.setItem('counter1', counter1.up());
-    mainContainer.innerText = `Задание (${arr[counter1.count - 1]})`;
-}
-
-if (!counter1.count) {
-    console.log('heheh', counter1.count);
-    f();
+mainButton1.addEventListener('click', switchGame1);
+mainButton2.addEventListener('click', () => {
+    if (counter1.count > 1) {
+        localStorage.setItem('counter1', counter1.down());
+        mainContainer.innerText = `Задание (${arr[counter1.count - 1]})`;
+    }
+});
+if (!counter.count) {
+    switchGame1();
 } else {
     mainContainer.innerText = `Задание (${arr[counter1.count - 1]})`;
 }
-mainButton2.addEventListener('click', () => {
-    localStorage.setItem('counter1', counter1.down());
-});
-
--- js/start.js
+++ js/start.js
@@ -3,4 +3,5 @@ let mainContainer = document.querySelector('.main__container');
 let mainButton1 = document.querySelector('.button1');
 let mainButton2 = document.querySelector('.button2');
 let cod = [123,456,789,1011,1213];
+let arrStart = ['Привет, это игра','Отлично, жми старт', "-", "+", "-", "+", "-" ];
 let arr = ['one','tow','three','four','five'];
